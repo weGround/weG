6ab85646e22edf5dad0a50dc7a7d7378edbc3d24
jihwan <dhwlghksy@gmail.com>
2023-07-12 15:43:14 +0900

    Feat : Make Post list with datas from server

--- app/src/main/java/com/example/weg/data/PostDataSource.kt
+++ app/src/main/java/com/example/weg/data/PostDataSource.kt
@@ -7,51 +7,51 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
 
 class PostDataSource {
     fun getAllPost(groupName: String?,callback: (Result<ArrayList<PostRecyclerItem>>) -> Unit) {
-        if(groupName == null){
+        if(groupName.equals("")){
             callback(Result.Error(IOException()));
-        }else {
-            var postList: ArrayList<PostRecyclerItem> = ArrayList<PostRecyclerItem>();
-            postList.add(PostRecyclerItem("Jihwan", "오늘의 한줄", "과연 내가 이걸 완성할 수 있을까", arrayListOf("jihwan","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("Yejin", "오늘의 한줄", "밥 먹었으니까 3시간 뒤에 잘거야", arrayListOf("사람1","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("SeungJong", "오늘의 한줄", "오늘은 질문하는 학생들이 적구나", arrayListOf("사람1","jihwan", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("Jihwan", "오늘의 한줄", "과연 내가 이걸 완성할 수 있을까", arrayListOf("사람1","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("Yejin", "오늘의 한줄", "밥 먹었으니까 3시간 뒤에 잘거야", arrayListOf("jihwan","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("SeungJong", "오늘의 한줄", "오늘은 질문하는 학생들이 적구나", arrayListOf("사람1","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("Jihwan", "오늘의 한줄", "과연 내가 이걸 완성할 수 있을까", arrayListOf("사람1","jihwan", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("Yejin", "오늘의 한줄", "밥 먹었으니까 3시간 뒤에 잘거야", arrayListOf("jihwan","사람2", "사람3", "사람4")));
-            postList.add(PostRecyclerItem("SeungJong", "오늘의 한줄", "오늘은 질문하는 학생들이 적구나", arrayListOf("사람1","사람2", "jihwan", "사람4")));
-            callback(Result.Success(postList));
+        }else{
+            val url = "http://172.10.5.148:443/share/getGroupPost/$groupName";
+
+            val client = OkHttpClient();
+
+            val request = Request.Builder()
+                .url(url)
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                    callback(Result.Error(IOException(e.message.toString(), e)))
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val responseBody: String = response.body?.string() ?: ""
+                    val jsonArray = JSONArray(responseBody);
+                    // JSON 배열을 반복하면서 객체 추출
+                    var memList: ArrayList<PostRecyclerItem> = ArrayList<PostRecyclerItem>();
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+
+                        // JSON 객체에서 필요한 값 가져오기
+                        val postWriter = jsonObject.getString("post_writer");
+                        val postTitle = jsonObject.getString("post_title");
+                        val postDetail = jsonObject.getString("post_detail");
+                        val likeUsers = jsonObject.getJSONArray("like_users");
+                        val likeUserList: ArrayList<String> =  ArrayList<String>();
+                        for (j in 0 until likeUsers.length()) {
+                            likeUserList.add(likeUsers.getString(j));
+                        }
+                        memList.add(PostRecyclerItem(postWriter, postTitle, postDetail, likeUserList))
+                    }
+                    callback(Result.Success(memList));
+                }
+            })
         }
-
-//        // TODO: handle loggedInUser authentication
-//        val url = "http://172.10.5.148:443/group/getMems/$groupName";
-
-//        val client = OkHttpClient();
-//
-//        val request = Request.Builder()
-//            .url(url)
-//            .build()
-
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                e.printStackTrace()
-//                callback(Result.Error(IOException(e.message.toString(), e)))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val responseBody: String = response.body?.string() ?: ""
-//                val jsonArray = JSONArray(responseBody);
-//                var memList : ArrayList<String> = ArrayList<String>();
-//                for (i in 0 until jsonArray.length()) {
-//                    memList.add(jsonArray.get(i).toString())
-//                }
-//                callback(Result.Success(memList));
-//            }
-//        })
     }
 
     fun addNewPost(postWriter:String?, postTitle: String?,postContent: String?,callback: (Result<PostRecyclerItem>) -> Unit) {
